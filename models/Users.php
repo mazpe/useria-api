ce07f50605073496092ce25cdbbb9dcae99f1d78
Lester Mesa <lesterm@gmail.com>
2019-05-14 12:05:04 -0400

    Users model with some minimal validation

---
+++ models/Users.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Useria\Users;
+
+use Phalcon\Mvc\Model;
+use Phalcon\Mvc\Model\Message;
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\Uniqueness;
+use Phalcon\Validation\Validator\InclusionIn;
+
+class Robots extends Model
+{
+    public function validation()
+    {
+        $validator = new Validation();
+        
+        // Type must be: master, admin or user
+        $validator->add(
+            "type",
+            new InclusionIn(
+                [
+                    'message' => 'Type must be "master", "admin", or "user"',
+                    'domain' => [
+                        'master',
+                        'admin',
+                        'user',
+                    ],
+                ]
+            )
+        );
+
+        // Username must be unique
+        $validator->add(
+            'username',
+            new Uniqueness(
+                [
+                    'field'   => 'username',
+                    'message' => 'The username must be unique',
+                ]
+            )
+        );
+
+        // Year cannot be less than zero
+        if ($this->year < 0) {
+            $this->appendMessage(
+                new Message('The year cannot be less than zero')
+            );
+        }
+
+        // Check if any messages have been produced
+        if ($this->validationHasFailed() === true) {
+            return false;
+        }
+    }
+}

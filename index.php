fa65a0d8aaaf1ff86949050cddb3dbb16585ca06
Lester Mesa <lesterm@gmail.com>
2019-05-14 12:12:07 -0400

    Loading model, db connection and binding DI

--- index.php
+++ index.php
@@ -1,9 +1,37 @@
 <?php
 
+use Phalcon\Loader;
 use Phalcon\Mvc\Micro;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Db\Adapter\Pdo\Mysql as PdoMysql;
 
-$app = new Micro();
+// Use Loader() to autoload our model
+$loader = new Loader();
 
-// Define the routes here
+$loader->registerNamespaces(
+    [
+        'Useria\Users' => __DIR__ . '/models/',
+    ]
+);
 
-$app->handle();
+$loader->register();
+
+$di = new FactoryDefault();
+
+// Set up the database service
+$di->set(
+    'db',
+    function () {
+        return new PdoMysql(
+            [
+                'host'     => 'localhost',
+                'username' => 'root',
+                'password' => 'root',
+                'dbname'   => 'useria',
+            ]
+        );
+    }
+);
+
+// Create and bind the DI to the application
+$app = new Micro($di);
